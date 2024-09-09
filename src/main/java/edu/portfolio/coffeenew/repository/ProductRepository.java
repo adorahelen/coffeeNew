73661a6d6349ebd6be9edda86208ecb82081dcfa
adorahelen <adorahelenmin@gmail.com>
2024-09-09 11:20:29 +0900

    ..

--- src/main/java/edu/portfolio/coffeenew/repository/ProductRepository.java
+++ src/main/java/edu/portfolio/coffeenew/repository/ProductRepository.java
@@ -1,7 +1,17 @@
 package edu.portfolio.coffeenew.repository;
 
+import edu.portfolio.coffeenew.entity.Category;
 import edu.portfolio.coffeenew.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Optional<Product> findByProductName(String productName);
+    List<Product> findByCategory(Category category);
+
+    // 테이블 이름이랑 맞춰야 한다. -> 키워드만으로 메소드를 자동 생성해줌
+
+
 }
