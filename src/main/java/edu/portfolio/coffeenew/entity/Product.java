22fb96b64b5ceda8ac0ca3507cd41814f90286ac
adorahelen <adorahelenmin@gmail.com>
2024-09-09 20:46:59 +0900

    ..

--- src/main/java/edu/portfolio/coffeenew/entity/Product.java
+++ src/main/java/edu/portfolio/coffeenew/entity/Product.java
@@ -20,6 +20,8 @@ public class Product {
     @Column(name = "product_id")
     private Long productId;
     // 처음에 오토인크리먼트 안해놔서 (이미 구현됨, 지금은 저거 넣으면 에러남)
+    // 이전에는 랜덤한 값이 들어갔는데, 왜 이번에는 오토 인크리먼트가 들어갔는지 모르겠다
+    //before : 220,552 등의 랜덤값 -> after : 1-2-3 (drop 한 다음 다시 만듬)
 
     @Column(name = "product_name", length = 20, nullable = false)
     private String productName;
