9df85119776efeb69b9fa1724f1382778411f539
adorahelen <adorahelenmin@gmail.com>
2024-09-09 11:17:46 +0900

    .

--- src/main/java/edu/portfolio/coffeenew/entity/Product.java
+++ src/main/java/edu/portfolio/coffeenew/entity/Product.java
@@ -8,6 +8,7 @@ import java.time.LocalDateTime;
 @Builder
 @Getter
 @Setter
+@ToString // 추가하면 Product@7a80ebcd -> productId=1252, productName=New
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
@@ -15,9 +16,10 @@ import java.time.LocalDateTime;
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue //(strategy = GenerationType.IDENTITY)
     @Column(name = "product_id")
     private Long productId;
+    // 처음에 오토인크리먼트 안해놔서 (이미 구현됨, 지금은 저거 넣으면 에러남)
 
     @Column(name = "product_name", length = 20, nullable = false)
     private String productName;
